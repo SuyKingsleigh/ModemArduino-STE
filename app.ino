c14ad24e92b6c9425a0f226735b72ae791ce8a23
SuyKingsleigh <suyan.m@aluno.ifsc.edu.br>
2020-05-31 15:58:35 -0300

    interface mac implementada

--- app.ino
+++ app.ino
@@ -2,11 +2,15 @@
 #include "src/ip_interface.h"
 #include "src/mac_interface.h"
 
+MacInterface mac; 
 
 void setup(){
     Serial.begin(9600);
+    delay(500);
+
+    mac = MacInterface();
 }
 
 void loop(){
-
+    mac.show_menu();
 }
