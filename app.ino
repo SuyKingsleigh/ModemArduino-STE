6b58ece0174c0c0d1c1cb44dfd6f10d36532939e
SuyKingsleigh <suyan.m@aluno.ifsc.edu.br>
2020-05-31 19:44:11 -0300

    canais implementados

--- app.ino
+++ app.ino
@@ -4,18 +4,18 @@
 #include "src/mac_interface.h"
 
 
-MacInterface mac; 
+MacInterface mac = MacInterface();
+ChannelInterface chan = ChannelInterface(11, 22, 2412); 
+
 
 void setup(){
     Serial.begin(9600);
     delay(500);
-
-    mac = MacInterface();
-
 }
 
+
 void loop(){
-    Serial.println("[1] Para interface MAC\n[2] Para interface IP\n");
+    Serial.println("[1] Para interface MAC\n[2] Para interface IP\n[3] Para interface dos canais");
 
     int select = -1;
 
@@ -27,6 +27,16 @@ void loop(){
             mac.show_menu();
             delay(125);
             return; 
+        
+        case 2:
+            Serial.println("ainda nao implementada");
+            delay(125);
+            return; 
+        
+        case 3: 
+            chan.show_menu();
+            delay(125);
+            return;
 
         default:
             return;
--- src/channel.h
+++ src/channel.h
@@ -1,27 +1,29 @@
 #if !defined(CHANNEL)
 #define CHANNEL
-#define BANDWIDTH 22 // largura de banda padrao 
 
 
 #include "Arduino.h"
+#include "menu.h"
 
 
 class Channel { 
     private:
+        int id; 
         int min_freq; 
         int max_freq; 
-        int central_frequency; 
         bool allocated; 
+        int central_frequency; 
     
     public:
-        Channel(int c_freq);
-        
+        Channel(int c_freq, int id, int bandwidth);
+
+        bool is_allocated();
+
         /** 
          * Aloca um canal 
          * Caso já esteja alocado retorna false 
         */
         bool allocate();
-
 
         /** 
          * Desaloca um canal
@@ -30,31 +32,31 @@ class Channel {
         bool deallocate();
 
         // fmin, fmax, fcentral e status 
-        const char * to_string(); 
+        String to_string(); 
 
 };
-
-
-struct ChannelTable {
-    int id; 
-    Channel chan; 
-};
-
 
 class ChannelInterface : public IMenu { 
-    private: 
-        struct ChannelTable table; 
+    private:
+        int num_channel; 
 
-        Channel get_channel(int id);
+        Channel * table; 
         
         bool allocate_chan(int id);
 
         bool deallocate_chan(int id);
 
-        bool get_chan_info(int id);
+        String get_chan_info(int id);
+
+        int selectChannel(); 
    
     public:
-        ChannelInterface();
+        /**
+         * num_channel = numero de canais 
+         * c_freq_distance = distancia da freq central de um canal para outro 
+         * ifreq = frequencia central do primeiro canal 
+        */
+        ChannelInterface(int num_channel, int c_freq_distance, int ifreq);
 
         // Metodos da classe parente 
         
