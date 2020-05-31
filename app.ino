a0f52af7f16d75cf94aafb69d2fb2b38348b4cd3
Renan Rodolfo da Silva <renaner123@gmail.com>
2020-05-31 17:44:23 -0300

    Update app.ino

--- app.ino
+++ app.ino
@@ -2,7 +2,36 @@
 #include "src/ip_interface.h"
 #include "src/mac_interface.h"
 
+MacInterface mac; 
 
-void setup(){}
+void setup(){
+    Serial.begin(9600);
+    delay(500);
 
-void loop(){}
+    mac = MacInterface();
+    ip = IpInterface();
+
+}
+
+void loop(){
+    Serial.println("[1] Para interface MAC\n[2] Para interface IP\n");
+
+    int select = -1;
+
+    while (select < 1) 
+        select = Serial.parseInt();
+    
+    switch (select) {
+        case 1:
+            mac.show_menu();
+            delay(125);
+            return; 
+        case 2:
+            ip.show_menu();
+            delay(125);
+            return; 
+
+        default:
+            return;
+    }
+}
