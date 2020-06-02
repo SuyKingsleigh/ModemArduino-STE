3559cf3b704ff701e27ad4392d71aefdb7cd328c
SuyKingsleigh <suyan.m@aluno.ifsc.edu.br>
2020-06-02 02:05:07 -0300

    interface de login implementada, placa trocada para arduino mega

--- src/login.h
+++ src/login.h
@@ -1,7 +1,6 @@
 #if !defined(LOGIN)
 #define LOGIN
 
-<<<<<<< HEAD
 #include "menu.h"
 #include "Arduino.h"
 
@@ -32,37 +31,6 @@ class Login{
 
         // retorna caso o usuario esteja autenticado
         bool is_authenticated(); 
-=======
-// #include "Arduino.h"
-#include <string>
-#include "menu.h"
-#include <iostream>
-
-using namespace std;
-
-class Login{
-    private:
-    // senha que pode ser alterada, padrão é ter 5 dígitos
-    string password;
-    // login padrão e único
-    string LOGINDEFAULT ="admin";
-    // senha para primeiro acesso ao modem
-    string PASSWORDDEFAULT = "admin";
-
-    public:
-  
-
-        Login();   
-        // retornar o usário padrão
-        string get_username();
-        // Alterar a senha de acesso ao modem
-        bool set_newPassword(string newPwd);
-        // Volta a senha para o padrão de fábrica        
-        bool resetPassword();
-        // validar credencias do usuario
-        bool log_in(string user,string pwd);
-
->>>>>>> 54c82d91e56ed8d820d11b6fd832a9fe4cb7166b
 };
 
 
@@ -70,17 +38,13 @@ class LoginInterface : public IMenu {
     private:
         Login login; 
 
-<<<<<<< HEAD
         // indica se o usuario está autenticado 
         bool status; 
 
-=======
->>>>>>> 54c82d91e56ed8d820d11b6fd832a9fe4cb7166b
     public:
         LoginInterface();
 
         ~LoginInterface() = default;
-<<<<<<< HEAD
 
         // Mostra o usuário para logar no modem
         void print_Login(); 
@@ -91,19 +55,10 @@ class LoginInterface : public IMenu {
         // Volta a senha para o padrão
         void reset_Password();
 
-=======
-        // Mostra o usuário para logar no modem
-        void print_Login(); 
-        // Altera a senha do modem
-        void update_Password();
-        // Volta a senha para o padrão
-        void reset_Password();
->>>>>>> 54c82d91e56ed8d820d11b6fd832a9fe4cb7166b
         // validar credencias do usario
         void log_in_interface();
         
         // Metodos da classe pai         
-<<<<<<< HEAD
         
         const char * get_info();
 
@@ -114,18 +69,3 @@ class LoginInterface : public IMenu {
 
 
 #endif // LOGIN
-=======
-        const char * get_info();
-
-        void show_menu();
-
-};
-
-
-
-
-
-
-
-#endif // LOGIN
->>>>>>> 54c82d91e56ed8d820d11b6fd832a9fe4cb7166b
